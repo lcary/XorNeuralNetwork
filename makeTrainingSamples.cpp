24690a15fc04436538cc9e4bcd9c35e125d51575
Luc Cary <luc.cary@gmail.com>
2017-10-15 01:00:18 -0400

    Reduce training sample count

--- makeTrainingSamples.cpp
+++ makeTrainingSamples.cpp
@@ -9,7 +9,7 @@ int main()
     // Random training sets for XOR -- two inputs and one output
 
     cout << "topology: 2 4 1" << endl;
-    for(int i = 100000; i >= 0; --i)
+    for(int i = 2000; i >= 0; --i)
     {
         int n1 = (int)(2.0 * rand() / double(RAND_MAX));
         int n2 = (int)(2.0 * rand() / double(RAND_MAX));
